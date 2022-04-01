fb982fdbd7cd4df4387dc9d4d211c571df929f03
spacekey <fumihiro.fukuda@gmail.com>
2022-04-01 15:53:41 +0800

    Update basic pub/sub test

--- tests/AlterNats.Tests/PubSubTest.cs
+++ tests/AlterNats.Tests/PubSubTest.cs
@@ -10,18 +10,13 @@ namespace AlterNats.Tests;
 public class PubSubTest
 {
     [Theory]
-    [InlineData(4222, 4222)]
-    [InlineData(4222, 4223)]
-    [InlineData(4223, 4222)]
-    [InlineData(4223, 4223)]
-    public async Task Basic(int subPort, int pubPort)
+    [MemberData(nameof(TestData))]
+    public async Task Basic<T>(int subPort, int pubPort, IEnumerable<T> items)
     {
-        var random = new Random();
-        var messages = Enumerable.Range(0, 10).Select(_ => random.Next(0, 100).ToString()).ToArray();
         AutoResetEvent autoResetEvent = new AutoResetEvent(false);
 
         autoResetEvent.Reset();
-        List<string> results = new();
+        List<T> results = new();
 
         var natsKey = new NatsKey(Guid.NewGuid().ToString());
 
@@ -32,11 +27,11 @@ public class PubSubTest
 
         await subConnection.ConnectAsync();
 
-        using var d = await subConnection.SubscribeAsync<string>(natsKey, x =>
+        using var d = await subConnection.SubscribeAsync<T>(natsKey, x =>
         {
             results.Add(x);
 
-            if (results.Count == 10)
+            if (results.Count == items.Count())
                 autoResetEvent.Set();
         });
 
@@ -47,27 +42,21 @@ public class PubSubTest
 
         await pubConnection.ConnectAsync();
 
-        foreach (var message in messages)
+        foreach (var item in items)
         {
-            await pubConnection.PublishAsync(natsKey, message);
+            await pubConnection.PublishAsync(natsKey, item);
         }
 
         var waitResult = autoResetEvent.WaitOne(3000);
 
         Assert.True(waitResult, "Timeout");
-        Assert.Equal(messages, results.ToArray());
+        Assert.Equal(items.ToArray(), results.ToArray());
     }
 
     [Theory]
-    [InlineData(4222, 4222)]
-    [InlineData(4222, 4223)]
-    [InlineData(4223, 4222)]
-    [InlineData(4223, 4223)]
-    public async Task BasicRequest(int subPort, int pubPort)
+    [MemberData(nameof(TestData))]
+    public async Task BasicRequest<T>(int subPort, int pubPort, IEnumerable<T> items)
     {
-        var random = new Random();
-        var messages = Enumerable.Range(0, 10).Select(_ => random.Next(0, 100).ToString()).ToArray();
-
         var natsKey = new NatsKey(Guid.NewGuid().ToString());
 
         await using var subConnection = new NatsConnection(NatsOptions.Default with
@@ -77,7 +66,7 @@ public class PubSubTest
 
         await subConnection.ConnectAsync();
 
-        using var d = await subConnection.SubscribeRequestAsync<string, string>(natsKey, x => $"Re{x}");
+        using var d = await subConnection.SubscribeRequestAsync<T, string>(natsKey, x => $"Re{x}");
 
         await using var pubConnection = new NatsConnection(NatsOptions.Default with
         {
@@ -86,9 +75,9 @@ public class PubSubTest
 
         await pubConnection.ConnectAsync();
 
-        foreach (var message in messages)
+        foreach (var item in items)
         {
-            Assert.Equal($"Re{message}", await pubConnection.RequestAsync<string, string>(natsKey, message));
+            Assert.Equal($"Re{item}", await pubConnection.RequestAsync<T, string>(natsKey, item));
         }
     }
 
@@ -102,5 +91,76 @@ public class PubSubTest
 
         await Assert.ThrowsAsync<SocketException>(async () => await connection1.ConnectAsync());
     }
+
+    static readonly int[] seed = { 24, 45, 99, 41, 98, 7, 81, 8, 26, 56 };
+
+    static object[][] TestData()
+    {
+        return new[]
+        {
+            new object[] { 4222, 4222, seed },
+            new object[] { 4222, 4223, seed },
+            new object[] { 4223, 4222, seed },
+            new object[] { 4223, 4223, seed },
+            new object[] { 4222, 4222, seed.Select(x => $"Test:{x}") },
+            new object[] { 4222, 4222, seed.Select(x => new SampleClass(x, $"Name{x}")) }
+        };
+    }
 }
 
+public class SampleClass : IEquatable<SampleClass>
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public SampleClass(int id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+
+    public bool Equals(SampleClass? other)
+    {
+        if (ReferenceEquals(null, other))
+        {
+            return false;
+        }
+
+        if (ReferenceEquals(this, other))
+        {
+            return true;
+        }
+
+        return Id == other.Id && Name == other.Name;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(null, obj))
+        {
+            return false;
+        }
+
+        if (ReferenceEquals(this, obj))
+        {
+            return true;
+        }
+
+        if (obj.GetType() != GetType())
+        {
+            return false;
+        }
+
+        return Equals((SampleClass)obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Id, Name);
+    }
+
+    public override string ToString()
+    {
+        return $"{Id}-{Name}";
+    }
+}
