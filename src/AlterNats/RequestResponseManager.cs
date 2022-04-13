d382530dac517f012fce078d034babf1b92359dc
neuecc <ils@neue.cc>
2022-04-13 17:38:36 +0900

    Improve RequestResponseManager part1

--- src/AlterNats/RequestResponseManager.cs
+++ src/AlterNats/RequestResponseManager.cs
@@ -1,25 +1,24 @@
 ﻿using AlterNats.Commands;
 using AlterNats.Internal;
 using System.Buffers;
-using System.Buffers.Text;
 using System.Text;
 
 namespace AlterNats;
 
-// TODO:make more...
+// TODO:when connection failed, can not receive response so Dispose All.
 internal sealed class RequestResponseManager : IDisposable
 {
     internal readonly NatsConnection connection;
     readonly ObjectPool pool;
     readonly object gate = new object();
+    readonly SemaphoreSlim asyncLock = new SemaphoreSlim(1, 1);
+    readonly CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
 
     int requestId = 0; // unique id per connection
-
-
-
+    bool isDisposed;
     // ID: Handler
     Dictionary<int, (Type responseType, object handler)> responseBoxes = new();
-    IDisposable? globalSubscription;
+    IDisposable? globalSubscription; // TODO:when connection failed, null and reuse???
 
     public RequestResponseManager(NatsConnection connection, ObjectPool pool)
     {
@@ -27,56 +26,82 @@ internal sealed class RequestResponseManager : IDisposable
         this.pool = pool;
     }
 
-
-
-    public async ValueTask<TResponse?> AddAsync<TRequest, TResponse>(NatsKey key, ReadOnlyMemory<byte> inBoxPrefix, TRequest request)
+    public ValueTask<TResponse?> AddAsync<TRequest, TResponse>(NatsKey key, ReadOnlyMemory<byte> inBoxPrefix, TRequest request)
     {
-        // TODO:lock...
-        var id = Interlocked.Increment(ref requestId);
-
-
-        var command = RequestAsyncCommand<TRequest, TResponse>.Create(pool, key, inBoxPrefix, id, request, connection.Options.Serializer);
-
-
-        // Subscribe connection wide inbox
         if (globalSubscription == null)
         {
-            var globalSubscribeKey = $"{Encoding.ASCII.GetString(inBoxPrefix.Span)}*";
-            globalSubscription = await connection.SubscribeAsync<byte[]>(globalSubscribeKey, _ => { });
+            return AddWithGlobalSubscribeAsync<TRequest, TResponse>(key, inBoxPrefix, request);
         }
 
+        return AddAsyncCore<TRequest, TResponse>(key, inBoxPrefix, request);
+    }
 
-        responseBoxes.Add(id, (typeof(TResponse), command));
+    async ValueTask<TResponse?> AddWithGlobalSubscribeAsync<TRequest, TResponse>(NatsKey key, ReadOnlyMemory<byte> inBoxPrefix, TRequest request)
+    {
+        await asyncLock.WaitAsync(cancellationTokenSource.Token);
+        try
+        {
+            if (globalSubscription == null)
+            {
+                var globalSubscribeKey = $"{Encoding.ASCII.GetString(inBoxPrefix.Span)}*";
+                globalSubscription = await connection.SubscribeAsync<byte[]>(globalSubscribeKey, _ => { }).ConfigureAwait(false);
+            }
+        }
+        finally
+        {
+            asyncLock.Release();
+        }
+
+        return await AddAsyncCore<TRequest, TResponse>(key, inBoxPrefix, request).ConfigureAwait(false);
+    }
+
+    ValueTask<TResponse?> AddAsyncCore<TRequest, TResponse>(NatsKey key, ReadOnlyMemory<byte> inBoxPrefix, TRequest request)
+    {
+        var id = Interlocked.Increment(ref requestId);
+        var command = RequestAsyncCommand<TRequest, TResponse?>.Create(pool, key, inBoxPrefix, id, request, connection.Options.Serializer);
+
+        lock (gate)
+        {
+            if (isDisposed) throw new NatsException("Connection is closed.");
+            responseBoxes.Add(id, (typeof(TResponse), command));
+        }
 
         connection.PostCommand(command);
-
-
-        // TODO:direct return
-        return await command.AsValueTask();
+        return command.AsValueTask();
     }
 
 
     public void PublishToResponseHandler(int id, in ReadOnlySequence<byte> buffer)
     {
-        // TODO:lock
-        if (responseBoxes.Remove(id, out var box))
+        (Type responseType, object handler) box;
+        lock (gate)
         {
-            ResponsePublisher.PublishResponse(box.responseType, connection.Options, buffer, box.handler);
+            if (!responseBoxes.Remove(id, out box))
+            {
+                return;
+            }
         }
+
+        ResponsePublisher.PublishResponse(box.responseType, connection.Options, buffer, box.handler);
     }
 
     public void Dispose()
     {
-        // throw new NotImplementedException();
+        if (isDisposed) return;
+        isDisposed = true;
+        cancellationTokenSource.Cancel();
 
-        foreach (var item in responseBoxes)
+        lock (gate)
         {
-            if (item.Value.handler is IPromise p)
+            foreach (var item in responseBoxes)
             {
-                p.SetCanceled(CancellationToken.None);
+                if (item.Value.handler is IPromise p)
+                {
+                    p.SetCanceled(CancellationToken.None);
+                }
             }
+            responseBoxes.Clear();
         }
-
     }
 
 }
